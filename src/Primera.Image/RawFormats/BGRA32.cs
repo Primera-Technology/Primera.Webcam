16a692d5f60ffe01512756fceac09959996945dc
eberg <Eberg@primera.com>
2023-07-12 10:23:46 -0500

    Webcam Library 1.0.1

--- src/Primera.Image/RawFormats/BGRA32.cs
+++ src/Primera.Image/RawFormats/BGRA32.cs
@@ -2,15 +2,4 @@
 
 namespace Primera.ImageTransform.RawFormats
 {
-    /// <summary>
-    /// 32-bit representation of RGB color with 8-bits each for R, G, B, and Alpha
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    public struct BGRA32
-    {
-        public byte B;
-        public byte G;
-        public byte R;
-        public byte A;
-    }
 }
